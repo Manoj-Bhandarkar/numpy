9a5928f46578d103709126171bab03a6c0189509
manoj-bhandarkar <developer.manojbhandarkar@gmail.com>
2026-07-28 17:14:35 +0530

    int value in ndarray

---
+++ 02_array_creation/01_scalar_to_ndarray.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+num = 10
+
+arr = np.array(num)
+
+print(arr)
+print(type(arr))
+
+print("ndim :", arr.ndim)
+print("shape:", arr.shape)
+print("size :", arr.size)
+print("dtype:", arr.dtype)
