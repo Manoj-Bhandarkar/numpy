6ad94140a8550a9f7c438d0afffc2e9526131f96
manoj-bhandarkar <developer.manojbhandarkar@gmail.com>
2026-07-28 17:15:51 +0530

    string value convert in ndarray

---
+++ 02_array_creation/02_sequence_to_ndarray.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+text = "PYTHON"
+
+arr = np.array(text)
+
+print(arr)
+print(type(arr))
+
+print("ndim :", arr.ndim)
+print("shape:", arr.shape)
+print("size :", arr.size)
+print("dtype:", arr.dtype)
