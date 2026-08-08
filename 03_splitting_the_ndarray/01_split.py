eff48a20a7b865db763cbfa9c070814505d0f246
manoj-bhandarkar <developer.manojbhandarkar@gmail.com>
2026-08-08 10:35:00 +0530

    Splitting NumPy array split()  examples

---
+++ 03_splitting_the_ndarray/01_split.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+a = np.arange(1, 13)
+
+print("Original Array:")
+print(a)
+
+result = np.split(a, 3)
+print(result)
+print("After Splitting:")
+for x in result:
+    print(x)
+    
+#output
+# [ 1  2  3  4  5  6  7  8  9 10 11 12]
+# [array([1, 2, 3, 4]), array([5, 6, 7, 8]), array([ 9, 10, 11, 12])]
+# After Splitting:
+# [1 2 3 4]
+# [5 6 7 8]
+# [ 9 10 11 12]
