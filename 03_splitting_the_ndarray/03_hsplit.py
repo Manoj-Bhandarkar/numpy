c8a0940e352c6c74f7f46b880a056ae9fb1e2c4e
manoj-bhandarkar <developer.manojbhandarkar@gmail.com>
2026-08-19 21:15:31 +0530

    Splitting NumPy hsplit() function

---
+++ 03_splitting_the_ndarray/03_hsplit.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+a = np.arange(1, 13).reshape(3, 4)
+
+print("Original Matrix:")
+print(a)
+
+result = np.hsplit(a, 2)
+
+print("After Horizontal Splitting:")
+
+for x in result:
+    print(x)
+
+# Original Matrix:
+# [[ 1  2  3  4]
+#  [ 5  6  7  8]
+#  [ 9 10 11 12]]
+
+# After Horizontal Splitting:
+
+# [[1 2]
+#  [5 6]
+#  [9 10]]
+
+# [[ 3  4]
+#  [ 7  8]
+#  [11 12]]
