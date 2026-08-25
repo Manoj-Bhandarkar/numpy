807de66deaab8426e857d4a8d3073316a19f1385
manoj-bhandarkar <developer.manojbhandarkar@gmail.com>
2026-08-25 19:54:23 +0530

    Add NumPy indexing with 2D arrays

---
+++ 04_numpy_indexing_and_slicing/03_indexing_2d.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+matrix = np.array([
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90]
+])
+
+print("Matrix:")
+print(matrix)
+
+# Access single elements
+print("\nmatrix[0, 0]:", matrix[0, 0])
+print("matrix[1, 1]:", matrix[1, 1])
+print("matrix[2, 2]:", matrix[2, 2])
+
+# Access rows
+print("\nFirst Row:", matrix[0])
+print("Second Row:", matrix[1])
+
+# Access columns
+print("\nFirst Column:", matrix[:, 0])
+print("Second Column:", matrix[:, 1])
+
+# Output Matrix:
+# [[10 20 30]
+#  [40 50 60]
+#  [70 80 90]]
+
+# matrix[0, 0]: 10
+# matrix[1, 1]: 50
+# matrix[2, 2]: 90
+
+# First Row: [10 20 30]
+# Second Row: [40 50 60]
+
+# First Column: [10 40 70]
+# Second Column: [20 50 80]
