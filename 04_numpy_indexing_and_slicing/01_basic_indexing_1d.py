e73d907c713d78af8e69a13f4975f6230a046af6
manoj-bhandarkar <developer.manojbhandarkar@gmail.com>
2026-08-25 19:49:04 +0530

    Add NumPy basic indexing with 1D array

---
+++ 04_numpy_indexing_and_slicing/01_basic_indexing_1d.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Creating 1-D ndarray
+arr = np.array([10, 20, 30, 40, 50])
+
+print("Original Array:", arr)
+
+# Positive indexing
+print("First element:", arr[0])
+print("Third element:", arr[2])
+
+# Negative indexing
+print("Last element:", arr[-1])
+print("Second last element:", arr[-2])
