d2e728898dac9322e9d4e820726435b36a6aedd3
manoj-bhandarkar <developer.manojbhandarkar@gmail.com>
2026-08-30 16:25:20 +0530

    Add iteration example for 1D NumPy array

---
+++ 05_iteration_of_ndarray/01_iterate_1d.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# Creating a 1-D array
+arr = np.array([10, 20, 30, 40, 50])
+
+print("Original Array:")
+print(arr)
+
+print("\nIterating Elements:")
+
+for value in arr:
+    print(value)
+    
+# Original Array:
+# [10 20 30 40 50]
+
+# Iterating Elements:
+# 10
+# 20
+# 30
+# 40
+# 50
