c22d574ef84932b4b650e0ca74b85927af9cfe63
manoj-bhandarkar <developer.manojbhandarkar@gmail.com>
2026-08-31 08:29:48 +0530

    Add element-wise arithmetic operations between NumPy arrays

---
+++ 06_arithmetic_operations/02_array_arithmetic.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+a = np.array([10, 20, 30, 40])
+b = np.array([2, 4, 5, 10])
+
+print("Array A:", a)
+print("Array B:", b)
+
+print("\nAddition:")
+print(a + b)
+
+print("\nSubtraction:")
+print(a - b)
+
+print("\nMultiplication:")
+print(a * b)
+
+print("\nDivision:")
+print(a / b)
+
+print("\nFloor Division:")
+print(a // b)
+
+print("\nModulus:")
+print(a % b)
+
+print("\nPower:")
+print(a ** b)
+
+# Array A: [10 20 30 40]
+# Array B: [ 2  4  5 10]
+
+# Addition:
+# [12 24 35 50]
+
+# Subtraction:
+# [ 8 16 25 30]
+
+# Multiplication:
+# [ 20  80 150 400]
+
+# Division:
+# [5. 5. 6. 4.]
+
+# Floor Division:
+# [5 5 6 4]
+
+# Modulus:
+# [0 0 0 0]
+
+# Power:
+# [              100            160000          24300000 10485760000000000]
