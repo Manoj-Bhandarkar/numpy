91394239c4497834d1113254dcfacda8f8017013
manoj-bhandarkar <developer.manojbhandarkar@gmail.com>
2026-08-31 08:28:26 +0530

    Add NumPy scalar arithmetic operations"

---
+++ 06_arithmetic_operations/01_scalar_operations.py
@@ -0,0 +1,61 @@
+
+
+import numpy as np
+
+# Creating NumPy array
+arr = np.array([10, 20, 30, 40, 50])
+
+print("Original Array:")
+print(arr)
+
+# Addition
+print("\nAddition (+10):")
+print(arr + 10)
+
+# Subtraction
+print("\nSubtraction (-5):")
+print(arr - 5)
+
+# Multiplication
+print("\nMultiplication (*2):")
+print(arr * 2)
+
+# Division
+print("\nDivision (/10):")
+print(arr / 10)
+
+# Floor Division
+print("\nFloor Division (//10):")
+print(arr // 10)
+
+# Modulus
+print("\nModulus (%3):")
+print(arr % 3)
+
+# Power
+print("\nPower (**2):")
+print(arr ** 2)
+
+# Original Array:
+# [10 20 30 40 50]
+
+# Addition (+10):
+# [20 30 40 50 60]
+
+# Subtraction (-5):
+# [ 5 15 25 35 45]
+
+# Multiplication (*2):
+# [ 20  40  60  80 100]
+
+# Division (/10):
+# [1. 2. 3. 4. 5.]
+
+# Floor Division (//10):
+# [1 2 3 4 5]
+
+# Modulus (%3):
+# [1 2 0 1 2]
+
+# Power (**2):
+# [ 100  400  900 1600 2500]
